a39767560f27ccf0718c7767ec6f3346acdfda36
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-01 15:52:20 +0530

    add post request

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000
 app.get('/', (request, response)=>{
     response.statusCode(200).send('OK')
 })
-app.get('/', (request, response)=>{
-    response.statusCode(200).send('OK')
+app.post('/', (request, response)=>{
+    response.statusCode(201).send('Created')
 })
 app.get('/', (request, response)=>{
     response.statusCode(200).send('OK')
