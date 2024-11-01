c998b9581fca587b2cdd136b1253c3d9ba971ac6
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-01 16:14:02 +0530

    add delete request

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (request, response)=>{
-    response.statusCode(200).send('OK')
-})
-app.post('/', (request, response)=>{
-    response.statusCode(201).send('Created')
-})
-app.put('/', (request, response)=>{
-    response.statusCode(202).send('Accepted')
-})
-app.get('/', (request, response)=>{
-    response.statusCode(200).send('OK')
-})
+app.get("/", (request, response) => {
+  response.statusCode(200).send("OK");
+});
+app.post("/", (request, response) => {
+  response.statusCode(201).send("Created");
+});
+app.put("/", (request, response) => {
+  response.statusCode(202).send("Accepted");
+});
+app.delete("/", (request, response) => {
+  response.statusCode(204).send("No Content");
+});
 
+app.listen(port, () => console.log("Server running on port ", port));
