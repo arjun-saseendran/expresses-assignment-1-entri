b6e249b32117046cdf70182dc83f9d18e414b111
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-01 16:18:09 +0530

    add donenv path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config({path: './.env'})
 const port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
